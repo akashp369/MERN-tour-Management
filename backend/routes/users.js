8c272955759ecc5c8f40a9a7d936c22554f6206f
akashp369 <aakashprajapat59@gmail.com>
2023-08-07 23:05:58 +0530

    all backend api done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,20 +2,20 @@ import express from 'express'
 import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/userController.js'
 const router =express.Router();
 
-import { verifyTokent } from '../utils/verifyToken.js';
+import { verifyTokent, verifyUser } from '../utils/verifyToken.js';
 
 
 
 // update User
-router.put('/:id',verifyTokent, updateUser)
+router.put('/:id',verifyUser, updateUser)
 
 // delete User
-router.delete('/:id',verifyTokent, deleteUser)
+router.delete('/:id',verifyUser, deleteUser)
 
 // get single User
-router.get('/:id',verifyTokent, getSingleUser)
+router.get('/:id',verifyUser, getSingleUser)
 
 // get all User
-router.get('/',verifyTokent, getAllUser)
+router.get('/',verifyUser, getAllUser)
 
 export default router
